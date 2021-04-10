d7fe8f56eb521fcf46a30f88f6e20185a5522e90
wDaniec <andrzejZdobywca@gmail.com>
2021-04-10 14:28:55 +0200

    Final version - i think

--- k_fold.py
+++ k_fold.py
@@ -3,13 +3,15 @@ import pandas as pd
 import os
 import sys
 import numpy as np
-from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import itertools
+from sklearn.metrics import accuracy_score, confusion_matrix
 from imblearn.over_sampling import SMOTE
 import neptune.new as neptune
 import optuna
 import logging
 
-K_FOLDS = 3
+K_FOLDS = 5
 IMPORTANT_FEATURES = 256
 
 dataset = pd.read_csv('../hackaton_input.csv', index_col=0)
@@ -25,7 +27,7 @@ dataset[contFeatures] = values
 subjects = list(dataset.subject.unique())
 labels = list(dataset.Activity.unique())
 final_test_subjects = [10,11,12]
-subjects = list(set(subjects) - set(final_test_subjects))
+subjects = list(set(subjects))
 final_test_dataset = dataset[dataset['subject'].isin(final_test_subjects)]
 dataset = dataset[dataset['subject'].isin(subjects)]
 
@@ -67,6 +69,41 @@ for fold_index in range(K_FOLDS):
 
   print(accuracy_score(test_y, preds_class))
 
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    fig, ax = plt.subplots(figsize=(10, 10))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    # plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
 def test(models):
   labels.sort()
   test_X = final_test_dataset[important_features].values
@@ -78,9 +115,13 @@ def test(models):
   predictions = np.array(predictions)
   preds = np.argmax(np.mean(predictions, axis=0), axis=1)
   final_test_preds = np.array([labels[pred] for pred in preds])
+
+  cnf_matrix = confusion_matrix(test_y, final_test_preds)
+  plt.figure()
+  plot_confusion_matrix(cnf_matrix, classes=sorted(dataset.Activity.unique()))
+  plt.savefig("siema")
   print(accuracy_score(test_y, final_test_preds))
 
 for idx, model in enumerate(models):
   model.save_model('model_{}_weights'.format(idx))
 test(models)
-
